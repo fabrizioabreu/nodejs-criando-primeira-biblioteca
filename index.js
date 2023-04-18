27b59cd27be28a33e4b9b7c5fcd0b6b3099d543e
fabrizioabreu <fabrizioabreu@live.com>
2023-04-17 21:53:16 -0300

    async/await

--- index.js
+++ index.js
@@ -5,16 +5,28 @@ function trataErro(erro) {
     throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
 }
 
-function pegaArquivo(caminhoDoArquivo) {
-    const encoding = 'utf-8';
-    fs.promises
-      .readFile(caminhoDoArquivo, encoding)
-      .then((texto) => console.log(chalk.green(texto)))
-      .catch(trataErro)
+// ---> async/await
+async function pegaArquivo(caminhoDoArquivo) {
+    try {
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+        console.log(chalk.green(texto));
+    } catch(erro) {
+        trataErro(erro)
+    }
 }
 
+// ---> Primises com then()
 // function pegaArquivo(caminhoDoArquivo) {
+//     const encoding = 'utf-8';
+//     fs.promises
+//       .readFile(caminhoDoArquivo, encoding)
+//       .then((texto) => console.log(chalk.green(texto)))
+//       .catch(trataErro)
+// }
 
+// ---> Versão Sincrona
+// function pegaArquivo(caminhoDoArquivo) {
 //     const encoding = 'utf-8';
 //     fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
 //         if(erro) {
