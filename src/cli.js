7987c2982268495ca5af353cf2032d7f23da83c6
fabrizioabreu <fabrizioabreu@live.com>
2023-04-19 21:47:42 -0300

    Refatorando entradas e saídas

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,14 @@
+import chalk from 'chalk';
 import pegaArquivo from './index.js';
 
 const caminho = process.argv;
 
-pegaArquivo(caminho[2]);
-// console.log(caminho[2]);
+async function processaTexto(caminho) {
+    const resultado = await pegaArquivo(caminho[2]);
+    console.log(chalk.yellow('lista de links'), resultado)
+}
 
+processaTexto(caminho);
+
+// console.log(caminho[2]);
 // node src/cli.js ./arquivos/texto.md
